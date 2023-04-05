0380de5adefc4f061b70b2519345d8b6aae0f473
Alejandro <aleepedro7@gmail.com>
2023-04-05 01:57:38 -0300

    first commit

---
+++ src/js/app.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function() {
+    darkMode();
+});
+
+function darkMode(){
+    const botonDarkMode = document.querySelector('.dark-mode-boton');
+
+    botonDarkMode.addEventListener('click', function(){
+        document.body.classList.toggle('dark-mode');
+    })
+}
